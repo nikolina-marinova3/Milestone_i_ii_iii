4a3d8db77d69e3d526410f9393d7a1c70d0ca090
nikolina-marinova3 <56189511+nikolina-marinova3@users.noreply.github.com>
2020-03-16 23:24:36 +0000

    Update github.js

--- githubfinder/github.js
+++ githubfinder/github.js
@@ -1,7 +1,7 @@
 class Github {
   constructor() {
     this.client_id = 'd9308aacf8b204d361fd';
-    this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
+    this.client_secret = '6feb149a21c640b5a40255bfb39d12af7f8d093f';
     this.repos_count = 5;
     this.repos_sort = 'created: asc';
   }
