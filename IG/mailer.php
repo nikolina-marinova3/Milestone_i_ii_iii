9ae36dc1a8a363f04bd824455035c410aa8fccfb
nikolina-marinova3 <56189511+nikolina-marinova3@users.noreply.github.com>
2020-04-17 20:12:46 +0100

    Update mailer.php

--- IG/mailer.php
+++ IG/mailer.php
@@ -1,18 +1,12 @@
 <?php
 
-    // Get the form fields, removes html tags and whitespace.
+    // Get the form fields, removes html tags and removes whitespace from customers entries.
     $name = strip_tags(trim($_POST["name"]));
     $name = str_replace(array("\r","\n"),array(" "," "),$name);
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $message = trim($_POST["message"]);
 
-    // Check the data.
-    if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: http://www.webdesigncourse.co/omnifood/index.php?success=-1#form");
-        exit;
-    }
-
-    // Set the recipient email address. Update this to YOUR desired email address.
+    // Set the recipient email address.
     $recipient = "nikolina_marinova@yahoo.com";
 
     // Set the email subject.
@@ -29,7 +23,4 @@
     // Send the email.
     mail($recipient, $subject, $email_content, $email_headers);
     
-    // Redirect to the index.html page with success code
-    header("Location: http://www.webdesigncourse.co/omnifood/index.php?success=1#form");
-
 ?>
